f10fc73d1eaad68e3403446144be6aa322950546
Patel <I25077@verisk.com>
2019-06-04 13:55:42 -0400

    added script

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+dt=`date '+%d-%m-%Y-%H-%M-%S'`
+git clone https://github.com/shivamXP/.net.git
+cd .net
+sudo docker build -t i25077 .
+$(sudo aws ecr get-login --no-include-email --region us-east-1)
+sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
+sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
+sudo aws cloudformation deploy --template-file ./cloudformation/service.yaml  --stack-name shivamServiceProd --parameter-overrides ImageTag=$dt --capabilities CAPABILITY_IAM --region us-east-1
+cd ..
+rm -rf .net
