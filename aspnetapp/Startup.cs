4f2152e5f3c65987e5864ac29a13dbe775774e4b
Patel <I25077@verisk.com>
2019-06-04 15:12:26 -0400

    removed https redirect

--- aspnetapp/Startup.cs
+++ aspnetapp/Startup.cs
@@ -48,7 +48,6 @@ namespace aspnetapp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
