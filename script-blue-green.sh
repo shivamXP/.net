a51b1e983e5a79dec21941e630adb58899d7df25
Patel <I25077@verisk.com>
2019-06-05 10:07:47 -0400

    bug fix

--- script-blue-green.sh
+++ script-blue-green.sh
@@ -6,7 +6,7 @@ sudo docker build -t i25077 .
 $(sudo aws ecr get-login --no-include-email --region us-east-1)
 sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
-sed 's/xxx/$dt/g' ./cloudformation/ServiceParameters.json
-aws cloudformation create-stack --stack-name shivamServicePreProd --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/ServiceParameters  --capabilities CAPABILITY_IAM --profile test
+sed "s/xxxx/$dt/g" "./cloudformation/ServiceParameters.json"
+aws cloudformation create-stack --stack-name shivamServicePreProd --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/ServiceParameters.json  --capabilities CAPABILITY_IAM --profile test
 cd ..
 rm -rf .net
