9eebf86d650ee3b22986a5b873cd59b3b48f20e6
Patel <I25077@verisk.com>
2019-06-05 18:50:01 -0400

    para3

--- cloudformation/script-In-place.sh
+++ cloudformation/script-In-place.sh
@@ -6,7 +6,7 @@ sudo docker build -t i25077 .
 $(sudo aws ecr get-login --no-include-email --region us-east-1)
 sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
-sudo sed -i "s/xxxx/$dt/g" "./cloudformation/prodServiceParameters.json"
+sudo sed -i "s/xxxx/$dt/g" "./cloudformation/prodServiceParameters.properties"
 sudo aws cloudformation deploy --template-file ./cloudformation/service.yaml  --stack-name shivamServiceProd --parameter-overrides $(cat ./cloudformation/prodServiceParameters.properties) --capabilities CAPABILITY_IAM --region us-east-1 --profile saml
 cd ..
 rm -rf .net
