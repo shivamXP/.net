7b2eb8a40d6586c75bcb6065b3474475506712b6
Patel <I25077@verisk.com>
2019-06-06 10:36:47 -0400

    as

--- cloudformation/script-In-place.sh
+++ cloudformation/script-In-place.sh
@@ -3,7 +3,7 @@ dt=`date '+%d-%m-%Y-%H-%M-%S'`
 git clone https://github.com/shivamXP/.net.git
 cd .net
 sudo docker build -t i25077 .
-$(sudo aws ecr get-login --no-include-email --region us-east-1)
+sudo $(sudo aws ecr get-login --no-include-email --region us-east-1)
 sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo sed -i "s/xxxx/$dt/g" "./cloudformation/prodServiceParameters.properties"
