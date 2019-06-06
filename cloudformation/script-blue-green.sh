ca65b8d9fbca45a6449e8ab137c0fda0b0599d38
Patel <I25077@verisk.com>
2019-06-06 10:47:20 -0400

    version2

--- cloudformation/script-blue-green.sh
+++ cloudformation/script-blue-green.sh
@@ -7,6 +7,6 @@ sudo $(sudo aws ecr get-login --no-include-email --region us-east-1)
 sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo sed -i "s/xxxx/$dt/g" "./cloudformation/ServiceParameters.json"
-sudo aws cloudformation create-stack --stack-name shivamServicePreProd --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/ServiceParameters.json  --capabilities CAPABILITY_IAM --profile saml --region us-east-1
+sudo aws cloudformation create-stack --stack-name $serviceName --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/ServiceParameters.json  --capabilities CAPABILITY_IAM --profile saml --region us-east-1
 cd ..
 rm -rf .net
