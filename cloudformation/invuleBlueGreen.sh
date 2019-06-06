fec597029abb678b7851abf96ecc2dbbad7bedc9
Patel <I25077@verisk.com>
2019-06-06 11:42:02 -0400

    version3

--- cloudformation/invuleBlueGreen.sh
+++ cloudformation/invuleBlueGreen.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/shivamXP/.net.git
 cd .net
 sudo sed -i "s/xxxx/$greenService/g" "./cloudformation/event.json"
+sudo sed -i "s/yyyy/$blueService/g" "./cloudformation/event.json"
 sudo aws lambda invoke --function-name shivamBlueGreen --invocation-type RequestResponse --payload  file://cloudformation/event.json  outfile.txt --region us-east-1 --profile saml
 cd ..
 rm -rf .net
