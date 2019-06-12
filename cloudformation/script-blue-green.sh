ef1a2f838dc6f5c22b260ad00138156fa86813f7
Patel <I25077@verisk.com>
2019-06-12 15:38:18 -0400

    minorUpdates

--- cloudformation/script-blue-green.sh
+++ cloudformation/script-blue-green.sh
@@ -1,7 +1,5 @@
 #!/bin/bash 
 dt=`date '+%d-%m-%Y-%H-%M-%S'`
-git clone https://github.com/shivamXP/.net.git
-cd .net
 commit=$(git log --oneline --format=%B -n 1 HEAD | head -n 1)
 sudo docker build -t i25077 .
 sudo $(sudo aws ecr get-login --no-include-email --region us-east-1)
@@ -10,5 +8,3 @@ sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo sed -i "s/xxxx/$dt/g" "./cloudformation/preProdServiceParameters.json"
 sudo sed -i "s/yyyy/$commit/g" "./cloudformation/preProdServiceParameters.json"
 sudo aws cloudformation create-stack --stack-name EcsService-$commit --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/preProdServiceParameters.json  --capabilities CAPABILITY_IAM --profile saml --region us-east-1
-cd ..
-rm -rf .net
