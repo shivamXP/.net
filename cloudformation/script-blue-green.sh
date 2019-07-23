1f2f9b527b5d5ede02597a07d6b7cf9271f09fd5
Patel <I25077@verisk.com>
2019-07-23 12:23:45 -0400

    v1

--- cloudformation/script-blue-green.sh
+++ cloudformation/script-blue-green.sh
@@ -7,4 +7,4 @@ sudo docker tag i25077:latest 063114128614.dkr.ecr.us-east-1.amazonaws.com/i2507
 sudo docker push 063114128614.dkr.ecr.us-east-1.amazonaws.com/i25077:$dt
 sudo sed -i "s/xxxx/$dt/g" "./cloudformation/preProdServiceParameters.json"
 sudo sed -i "s/yyyy/$commit/g" "./cloudformation/preProdServiceParameters.json"
-sudo aws cloudformation create-stack --stack-name EcsService-$commit --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/preProdServiceParameters.json  --capabilities CAPABILITY_IAM --profile saml --region us-east-1
+sudo aws cloudformation create-stack --stack-name SequelDemoService-$commit --template-body file://cloudformation/service.yaml --parameters=file://cloudformation/preProdServiceParameters.json  --capabilities CAPABILITY_IAM --profile saml --region us-east-1
